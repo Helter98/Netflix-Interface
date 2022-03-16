c1818df408471f175914c12b89489bb6e649d6bb
helter.xavier@hotmail.com <helter.xavier@hotmail.com>
2022-03-16 14:08:08 -0400

    .

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,14 +1,17 @@
 $('.owl-carousel').owlCarousel({
-    stagePadding: -80,
+    stagePadding: -300,
     loop:true, //efeito pra ta sempre girando
     margin:320, //alterar a margem entre um e outro
     nav:false,
     responsive:{
         0:{
-            items:1
+            items:4
         },
         600:{
-            items:4
+            items:5
+        },
+        900:{
+            items:6
         },
         1000:{
             items:7
